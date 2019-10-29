8403246496207515658a3ae3ede7eb25b1266d40
Caroline Chen <carolinecaroline@gmail.com>
2019-10-29 15:39:59 -0400

    set up Mutation and Query resolvers

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,17 @@
-const Mutations = {};
+//ctx.db is how we access the database
+
+const Mutations = {
+	async createAccount(parent, args, ctx, info) {
+		//TODO: check if they are logged in
+
+		const account = await ctx.db.mutation.createAccount({
+			data: {
+				...args
+			}
+		}, info);
+
+		return account;
+	}
+};
 
 module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,14 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+	items: forwardto('db'),
+	// forward to removes need for this code
+
+	//  async accounts(parent, args, ctx, info) {
+	// 	console.log("Getting accounts")
+	// 	const items = await ctx.db.query.accounts();
+	// 	return accounts;
+	// }
+};
 
 module.exports = Query;
