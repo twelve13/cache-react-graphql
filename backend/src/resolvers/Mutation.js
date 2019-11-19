9ebd1e164c7948dd0c3240f123e2565826f09c21
Caroline Chen <carolinecaroline@gmail.com>
2019-11-19 12:01:01 -0500

    add ability to update account

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -11,7 +11,23 @@ const Mutations = {
 		}, info);
 
 		return account;
-	}
+	},
+	updateAccount(parent, args, ctx, info) {
+		//first take a copy of the updates
+		const updates = { ...args };
+		//remove the ID from the updates bc you cant update the id
+		delete updates.id;
+		//run the update method
+		return ctx.db.mutation.updateAccount(
+			{	
+				data: updates,
+				where: {
+					id: args.id,
+				},
+			},
+			info
+		);
+	},
 };
 
 module.exports = Mutations;
